4506cfed6864a02ffe96a3ece18ef644cdb76c6c
김도영 <doyoung0205@naver.com>
2021-05-19 22:44:30 +0900

    feature: gradle의 codegen config

---
+++ breadgood-server/README.md
@@ -0,0 +1,6 @@
+# breadgood-server
+
+## codegen 
+
+``./gradlew generateSwaggerCode``
+
---
+++ breadgood-server/src/main/java/com/bside/breadgood/BreadgoodServerApplication.java
@@ -0,0 +1,13 @@
+package com.bside.breadgood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadgoodServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreadgoodServerApplication.class, args);
+    }
+
+}
