c11474e440734e38da8019180c0518103fc2e4e1
LJB GITHUB <32535590+bell204@users.noreply.github.com>
2021-05-31 11:18:53 +0900

    Delete scripts directory

--- scripts/test.sh
+++
@@ -1,7 +0,0 @@
-if [ -f ./breadgood-server/build/libs/breadgood-server-0.0.1-SNAPSHOT.jar ]; then
-  echo "File Exist !!!";
-fi
-
-if [ ! -f ./breadgood-server/build/libs/breadgood-server-0.0.1-SNAPSHOT.jar ]; then
-  echo "Not Exists";
-fi
