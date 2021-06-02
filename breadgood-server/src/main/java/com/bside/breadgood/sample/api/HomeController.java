b63e3dba13969c9208b617f99d7b579f8f620efc
bell2017 <bell2017@naver.com>
2021-06-02 23:03:25 +0900

    [ISSUE] 카카오 로그인, 로그아웃 연동 완료 (사용자 정보중 email은 왜 못 가져오는지 확인중)

---
+++ breadgood-server/src/main/java/com/bside/breadgood/sample/api/HomeController.java
@@ -0,0 +1,44 @@
+package com.bside.breadgood.sample.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+
+@RestController
+public class HomeController {
+
+    KakaoApi kakaoApi = new KakaoApi();
+
+    @RequestMapping(value="/login")
+    public ModelAndView login(@RequestParam("code") String code, HttpSession session) {
+        ModelAndView mav = new ModelAndView();
+        String accessToken = kakaoApi.getAccessToken(code);
+        HashMap<String, Object> userInfo = kakaoApi.getUserInfo(accessToken);
+
+        System.out.println("login info :" + userInfo.toString());
+
+        if(userInfo.get("nickname") != null) {
+//            session.setAttribute("userId", userInfo.get("email"));
+            session.setAttribute("access_token", accessToken);
+        }
+        mav.addObject("userId", userInfo.get("nickname"));
+        mav.setViewName("index");
+
+        return mav;
+    }
+
+    @RequestMapping(value ="/logout")
+    public ModelAndView logout(HttpSession session) {
+        ModelAndView mav = new ModelAndView();
+
+        kakaoApi.kakaoLogout((String)session.getAttribute("access_token"));
+        session.removeAttribute("access_token");
+        session.removeAttribute("userId");
+        mav.setViewName("index");
+        return mav;
+    }
+}
